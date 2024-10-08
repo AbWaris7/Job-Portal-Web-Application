b0412b78dda51b039e5489dba1a5f6f60aca2b45
abdulwariskhan <hellokhan.ch@gmail.com>
2024-10-08 21:22:08 +0200

    Add list of public URLs

--- src/main/java/com/khan/programmer/Job/Portal/config/WebSecurityConfig.java
+++ src/main/java/com/khan/programmer/Job/Portal/config/WebSecurityConfig.java
@@ -8,6 +8,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class WebSecurityConfig {
 
+    private final String[] publicUrl = {"/",
+            "/global-search/**",
+            "/register",
+            "/register/**",
+            "/webjars/**",
+            "/resources/**",
+            "/assets/**",
+            "/css/**",
+            "/summernote/**",
+            "/js/**",
+            "/*.css",
+            "/*.js",
+            "/*.js.map",
+            "/fonts**", "/favicon.ico", "/resources/**", "/error"};
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http){
