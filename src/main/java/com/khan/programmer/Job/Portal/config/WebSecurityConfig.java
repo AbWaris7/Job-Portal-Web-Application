73f1ebade588b097dfada2ad0edccb96045af07d
abdulwariskhan <hellokhan.ch@gmail.com>
2024-10-08 21:20:35 +0200

    Add SecurityFilterChain

--- src/main/java/com/khan/programmer/Job/Portal/config/WebSecurityConfig.java
+++ src/main/java/com/khan/programmer/Job/Portal/config/WebSecurityConfig.java
@@ -1,7 +1,16 @@
 package com.khan.programmer.Job.Portal.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
+
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http){
+        return http.build();
+    }
 }
