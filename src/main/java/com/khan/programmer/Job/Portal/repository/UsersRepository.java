aa06d9d15af68bc0d6f40ea9ceeb55bb4bb4144e
abdulwariskhan <hellokhan.ch@gmail.com>
2024-10-15 18:02:14 +0200

    Add UsersRepository to manage user data interactions

---
+++ src/main/java/com/khan/programmer/Job/Portal/repository/UsersRepository.java
@@ -0,0 +1,8 @@
+package com.khan.programmer.Job.Portal.repository;
+
+import com.khan.programmer.Job.Portal.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface UsersRepository extends JpaRepository<Users, Integer> {
+}
