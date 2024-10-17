123cc83ddbd80a5dcf1cfde364bb40450196ffbe
abdulwariskhan <hellokhan.ch@gmail.com>
2024-10-17 22:54:23 +0200

    Added @PostMapping for user registration at "/register/new"

--- src/main/java/com/khan/programmer/Job/Portal/controller/UsersController.java
+++ src/main/java/com/khan/programmer/Job/Portal/controller/UsersController.java
@@ -2,11 +2,14 @@ package com.khan.programmer.Job.Portal.controller;
 
 import com.khan.programmer.Job.Portal.entity.Users;
 import com.khan.programmer.Job.Portal.entity.UsersType;
+import com.khan.programmer.Job.Portal.services.UserService;
 import com.khan.programmer.Job.Portal.services.UsersTypeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -15,9 +18,11 @@ public class UsersController {
 
 
     private final UsersTypeService usersTypeService;
+    private final UserService userService;
     @Autowired
-    public UsersController(UsersTypeService usersTypeService) {
+    public UsersController(UsersTypeService usersTypeService, UserService userService) {
         this.usersTypeService = usersTypeService;
+        this.userService = userService;
     }
 
     @GetMapping("/register")
@@ -28,5 +33,12 @@ public class UsersController {
         model.addAttribute("user",new Users());
         return "register";
 
+    }
+
+    @PostMapping("/register/new")
+    public String userRegistration(@Valid Users users){
+
+        userService.addNew(users);
+        return "dashboard";
     }
 }
