4bbf3c45e408b2762530c4f3ec0476f41d064742
abdulwariskhan <hellokhan.ch@gmail.com>
2024-10-18 22:53:11 +0200

    add method to find users by email

--- src/main/java/com/khan/programmer/Job/Portal/repository/UsersRepository.java
+++ src/main/java/com/khan/programmer/Job/Portal/repository/UsersRepository.java
@@ -4,5 +4,10 @@ import com.khan.programmer.Job.Portal.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<Users, Integer> {
+
+
+    Optional<Users> findByEmail(String email);
 }
