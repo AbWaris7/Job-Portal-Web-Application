bca9a5588ea01bb4b9e94f7bfb017d5d2773485e
abdulwariskhan <hellokhan.ch@gmail.com>
2024-10-19 21:06:24 +0200

    Add JobSeekerProfileRepository for database access to JobSeekerProfile entity

---
+++ src/main/java/com/khan/programmer/Job/Portal/repository/JobSeekerProfileRepository.java
@@ -0,0 +1,7 @@
+package com.khan.programmer.Job.Portal.repository;
+
+import com.khan.programmer.Job.Portal.entity.JobSeekerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobSeekerProfileRepository  extends JpaRepository<JobSeekerProfile, Integer> {
+}
