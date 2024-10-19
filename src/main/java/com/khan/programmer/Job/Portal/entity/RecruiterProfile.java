ad8ea2987342a3032d98ee3e919cea1ed56fd777
abdulwariskhan <hellokhan.ch@gmail.com>
2024-10-19 21:06:48 +0200

    Add RecruiterProfileRepository for managing RecruiterProfile entity persistence

--- src/main/java/com/khan/programmer/Job/Portal/entity/RecruiterProfile.java
+++ src/main/java/com/khan/programmer/Job/Portal/entity/RecruiterProfile.java
@@ -36,6 +36,10 @@ public class RecruiterProfile {
         this.profilePhoto = profilePhoto;
     }
 
+    public RecruiterProfile(Users users) {
+        this.userId=users;
+    }
+
     public int getUserAccountId() {
         return userAccountId;
     }
---
+++ src/main/java/com/khan/programmer/Job/Portal/repository/RecruiterProfileRepository.java
@@ -0,0 +1,8 @@
+package com.khan.programmer.Job.Portal.repository;
+
+import com.khan.programmer.Job.Portal.entity.RecruiterProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
+
+}
