550ace50deffda305b8485a522add51f932ec9cc
abdulwariskhan <hellokhan.ch@gmail.com>
2025-04-16 06:55:19 +0200

    - Implemented HomeController to handle the root request mapping. - Created index.html template using Thymeleaf for the homepage view.

---
+++ src/main/java/com/khan/code/Job/Portal/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.khan.code.Job.Portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
